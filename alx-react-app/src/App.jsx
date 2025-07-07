12ae6e5b389d192f3cc6211dace8ef15416ba8e4
Barkotfrew <141666169+Barkotfrew@users.noreply.github.com>
2025-07-08 00:38:08 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,12 @@
-function WelcomeMessage() {
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
   return (
     <div>
-      <h1>Hello everyone, I am learning React at ALX!</h1>
-      <p>This is a simple JSX component.</p>
-      <p>I am learning about JSX!</p>
+      <WelcomeMessage />
     </div>
   );
 }
 
-export default WelcomeMessage;
-
-
+export default App;
