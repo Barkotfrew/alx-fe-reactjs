e54aa460cad1c454081a94b4e6b7a3947db5be24
Yeabsira Taye <ymttaye@gmail.com>
2025-08-24 18:54:05 +0300

    Update TodoList.jsx

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -10,40 +10,25 @@ const TodoList = () => {
   const [todos, setTodos] = useState(initialTodos);
 
   const addTodo = (text) => {
-    const newTodo = { id: Date.now(), text, completed: false };
-    setTodos([...todos, newTodo]);
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
   };
 
   const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
   };
 
   const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    setTodos(todos.filter(todo => todo.id !== id));
   };
 
   return (
     <div>
       <AddTodoForm addTodo={addTodo} />
       <ul>
-        {todos.map((todo) => (
-          <li
-            key={todo.id}
-            onClick={() => toggleTodo(todo.id)}
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none",
-              cursor: "pointer",
-              display: "flex",
-              justifyContent: "space-between",
-              marginBottom: "5px"
-            }}
-          >
+        {todos.map(todo => (
+          <li key={todo.id} onClick={() => toggleTodo(todo.id)} style={{ textDecoration: todo.completed ? "line-through" : "none", cursor: "pointer", display: "flex", justifyContent: "space-between", marginBottom: "5px" }}>
             <span>{todo.text}</span>
-            <button onClick={(e) => { e.stopPropagation(); deleteTodo(todo.id); }}>Delete</button>
+            <button onClick={e => { e.stopPropagation(); deleteTodo(todo.id); }}>Delete</button>
           </li>
         ))}
       </ul>
